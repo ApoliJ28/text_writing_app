287afaef72166ab90c33888adec5a2c4a7983946
ApoliJ28 <apolinaresvictor@gmail.com>
2024-08-17 11:49:26 -0400

    feat: project finnally.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Welcome</h1>"
+    return render_template('index.html')
+
+@app.route('/writing')
+def writing():
+    return render_template('writing.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
